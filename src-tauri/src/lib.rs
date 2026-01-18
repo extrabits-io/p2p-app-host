82e6d452e7402dc52880845fcb83310d3b202eda
Jamin Menter <jaminmenter@gmail.com>
2026-01-18 14:54:44 -0700

    add react-router skeleton app

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -19,35 +19,18 @@ struct AppState {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     env_logger::init();
-
-    let relay_config = RelayConfig {
-        address_range: "10.8.0.0/24".into(),
-        ip_address: "10.8.0.2".into(),
-        private_key: "<your private key>".into(),
-        relay_endpoint: "<relay IP or DNS>".into(),
-        relay_public_key: "<relay public key>".into(),
-    };
-    let relay_connection = relay::connect(&relay_config).unwrap();
-
-    let builder = tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .setup(move |app| {
-            let state = AppState { relay_connection };
-            app.manage(Mutex::new(state));
-
-            let server_config = SpaConfig {
-                host: format!("{}:{}", &relay_config.ip_address, 3000),
-                path: app.path().resolve("app/", BaseDirectory::Resource).unwrap(),
-            };
-            async_runtime::spawn(async move {
-                server::static_files::serve_spa(&server_config)
-                    .await
-                    .unwrap();
-            });
+    tauri::Builder::default()
+        .setup(|app| {
+            let salt_path = app
+                .path()
+                .app_local_data_dir()
+                .expect("could not resolve app local data path")
+                .join("salt.txt");
+            app.handle()
+                .plugin(tauri_plugin_stronghold::Builder::with_argon2(&salt_path).build())?;
             Ok(())
-        });
-
-    builder
+        })
+        .plugin(tauri_plugin_opener::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
